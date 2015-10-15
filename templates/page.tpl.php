28956124a9c9e04c4ed312ccc242677cfe557e51
George Petridis <george@georgepetridis.com>
2015-10-15 12:20:32 -0400

    Create page.tpl.php

---
+++ templates/page.tpl.php
@@ -0,0 +1,23 @@
+<div id="page" class="group">
+
+    <div id="top-bar" class="group">
+        <?php print render($page['top_bar']); ?>
+    </div><!-- /#slideshow -->
+    
+    <div id="header" class="group">
+        <?php print render($page['header']); ?>
+    </div><!-- /#header -->
+
+    <div id="main-content" class="group">
+        <?php print render($page['content']); ?>
+    </div><!-- /#main-content -->
+
+    <div id="footer" class="group">
+        <?php print render($page['footer']); ?>
+    </div><!-- /#footer -->
+
+    <div id="sub-footer" class="group">
+        <?php print render($page['sub_footer']); ?>
+    </div><!-- /#sub-footer -->
+
+</div><!-- /#page -->
